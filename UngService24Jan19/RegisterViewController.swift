e972485cdf450357d73dbc4d0522d6fd1dfe965e
masterUNG <phrombutr@gmail.com>
2019-01-24 14:16:19 +0700

    Segue Go Back Success

---
+++ UngService24Jan19/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  UngService24Jan19
+//
+//  Created by MasterUNG on 24/1/2562 BE.
+//  Copyright © 2562 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
