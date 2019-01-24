90282fd267969be318d1a64bd67288f4fb1d30ed
masterUNG <phrombutr@gmail.com>
2019-01-24 11:46:30 +0700

    Initial Commit

---
+++ UngService24Jan19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UngService24Jan19
+//
+//  Created by MasterUNG on 24/1/2562 BE.
+//  Copyright © 2562 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
