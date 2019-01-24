0c9c012a91772a157aecad44e330c36da84b51c1
masterUNG <phrombutr@gmail.com>
2019-01-24 14:57:18 +0700

    Create Outlet and Action Success

--- UngService24Jan19/RegisterViewController.swift
+++ UngService24Jan19/RegisterViewController.swift
@@ -9,22 +9,28 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }   // Main Method
+    
+    
+    @IBAction func uploadButton(_ sender: UIBarButtonItem) {
     }
     
+    
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+}   // Main Class
