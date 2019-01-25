b9bcbe4d3b451af7f62364d7056df49ba7879e9c
masterUNG <phrombutr@gmail.com>
2019-01-25 16:03:25 +0700

    finish

--- UngService24Jan19/ServiceViewController.swift
+++ UngService24Jan19/ServiceViewController.swift
@@ -7,24 +7,83 @@
 //
 
 import UIKit
+import MapKit
+
+
+
 
 class ServiceViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//       showMap(lat: 13.667693, lng: 100.621781)
+        
+        showMapType2(lat: 13.667693, lng: 100.621781)
+        
+    }   // Main Method
+    
+    func showMapType2(lat: Double, lng: Double) -> Void {
+        
+        let latitude:CLLocationDegrees = lat
+        let longtitude:CLLocationDegrees = lng
+        
+        let laDelta:CLLocationDegrees = 0.01
+        let longDelta:CLLocationDegrees = 0.01
+        
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: laDelta, longitudeDelta: longDelta)
+        let homeMaster: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: homeMaster, span: span)
+        
+        
+        self.myMapView.setRegion(region, animated: true)
+        
+        
+//        Home Master Anotation
+        let homeAnotation = MKPointAnnotation()
+        homeAnotation.coordinate = homeMaster
+        homeAnotation.title = "Home Master UNG"
+        homeAnotation.subtitle = "EWTC Institute"
+        self.myMapView.addAnnotation(homeAnotation)
+        
+//        Bus Stop
+        
+        let latBus:CLLocationDegrees = 13.669372
+        let longBus:CLLocationDegrees = 100.624802
+        
+        let busStopAnotation = MKPointAnnotation()
+        busStopAnotation.coordinate = CLLocationCoordinate2D(latitude: latBus, longitude: longBus)
+        busStopAnotation.title = "Bus Stop"
+        busStopAnotation.subtitle = "Bus StopBus StopBus StopBus StopBus StopBus StopBus Stop"
+        self.myMapView.addAnnotation(busStopAnotation)
+        
+        
+        
+        
+        
+    }
+    
+    func showMap(lat: Double, lng: Double) -> Void {
+        
+        let homeCoordinate = CLLocationCoordinate2DMake(lat, lng)
+        let regionDistance: CLLocationDistance = 1000
+        let regionSpan = MKCoordinateRegion.init(center: homeCoordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
+        let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
+        let placeMark = MKPlacemark(coordinate: homeCoordinate)
+        let mapItem = MKMapItem(placemark: placeMark)
+        mapItem.name = "Home Master"
+        mapItem.openInMaps(launchOptions: options)
+        
     }
     
 
-    /*
-    // MARK: - Navigation
+   
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+}   // Main Class
