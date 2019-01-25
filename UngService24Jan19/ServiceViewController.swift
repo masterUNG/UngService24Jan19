93f628a0f6694979d1068935d98a7b8cd1dd0cd4
masterUNG <phrombutr@gmail.com>
2019-01-25 11:10:50 +0700

    Outlet and Action Main Success

---
+++ UngService24Jan19/ServiceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ServiceViewController.swift
+//  UngService24Jan19
+//
+//  Created by MasterUNG on 25/1/2562 BE.
+//  Copyright © 2562 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ServiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- UngService24Jan19/ViewController.swift
+++ UngService24Jan19/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }   // Main Method
+    
+    
+    
+    @IBAction func loginButton(_ sender: UIButton) {
     }
-
+    
 
 }
 
